3c52ae14ce34f6b29f248921dcc802c028ad33a3
Paulo Soares <pauloxti@gmail.com>
2019-09-16 19:30:31 +0200

    test :bug:

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
-
-
-
-
 import (
 	"github.com/nikola43/go_github/first_package"
 	"github.com/nikola43/go_github/second_package"
 	"github.com/nikola43/go_github/services"
 )
-
 func main() {
 	services.CreateGithubRepository()
 	services.FindGithubRepository()
